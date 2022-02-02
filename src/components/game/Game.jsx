1d057c0f238cafa14b429ca022bfc5c9d389f01c
josehfortes <jose@seja.dev>
2022-02-02 15:40:37 -0300

    DESAFIO: placar

--- src/components/game/Game.jsx
+++ src/components/game/Game.jsx
@@ -22,6 +22,8 @@ function Game () {
   const [winner, setWinner] = useState(0)
   const [winnerLine, setWinnerLine] = useState([])
   const [draw, setDraw] = useState(false)
+  const [xWinnerTimes, setXWinnerTimes] = useState(0)
+  const [circleWinnerTimes, setCircleWinnerTimes] = useState(0)
 
   const handleClick = (pos) => {
     if (gameState[pos] === 0 && winner === 0) {
@@ -38,6 +40,11 @@ function Game () {
       if (sum === 3 || sum === -3) {
         setWinner(sum / 3)
         setWinnerLine(line)
+        if (sum > 0) {
+          setCircleWinnerTimes(circleWinnerTimes + 1)
+        } else {
+          setXWinnerTimes(xWinnerTimes + 1)
+        }
       }
     })
   }
@@ -91,7 +98,10 @@ function Game () {
           isDraw={draw}
         />
       </div>
-      <Score />
+      <Score 
+        xWinnerTimes={xWinnerTimes}
+        circleWinnerTimes={circleWinnerTimes}
+      />
     </>
   )
 }
